5b65b5d93fd4177dd77254b7a97c31e66626be07
konuralp <konuralpsenoglu@hotmail.com>
2019-02-02 15:48:01 +0300

    Getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,17 @@
 package com.mycompany.app;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 
 /**
  * Hello world!
@@ -9,14 +19,87 @@ import java.util.Arrays;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        ArrayList<Integer> aList = new ArrayList<Integer>(Arrays.asList(new Integer[]{1,2,3,4,5}));
-        ArrayList<String> strList = new ArrayList<String>(Arrays.asList(new String[]{"Fatsani","Ayanda","Adebayo","Mtendere","Azariel"}));
-        ArrayList<Integer> aList2 = new ArrayList<Integer>(Arrays.asList(new Integer[]{5,4,3,2,1}));
-        ArrayList<String> strList2 = new ArrayList<String>(Arrays.asList(new String[]{"Moontoes","SanguineBane","Sweetdrop","Quickglow","Twirlcloud"}));
-        System.out.println(repeat(aList,aList2,strList,strList2));
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+            //System.out.println(req.queryParams("input1"));
+            //System.out.println(req.queryParams("input2"));
+
+            String input1 = req.queryParams("strList1");
+            java.util.Scanner sc1 = new java.util.Scanner(input1);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<String> strList1 = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                strList1.add(sc1.next().replaceAll("\\s",""));
+            }
+            System.out.println(strList1);
+            sc1.close();
+
+            String input2 = req.queryParams("strList2");
+            java.util.Scanner sc2 = new java.util.Scanner(input2);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<String> strList2 = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                strList2.add(sc1.next().replaceAll("\\s",""));
+            }
+            sc2.close();
+            System.out.println(strList2);
+
+
+            String input3 = req.queryParams("intList1");
+            java.util.Scanner sc3 = new java.util.Scanner(input2);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> intList1 = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+                intList1.add(value);
+            }
+            System.out.println(strList2);
+            sc3.close();
+
+            String input4 = req.queryParams("intList2");
+            java.util.Scanner sc4 = new java.util.Scanner(input2);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> intList2 = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+                intList2.add(value);
+            }
+            System.out.println(strList2);
+            sc4.close();
+
+            String result = App.repeat(intList1,intList2,strList1,strList2);
+
+            Map map = new HashMap();
+            map.put("result", result);
+            return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+                (rq, rs) -> {
+                    Map map = new HashMap();
+                    map.put("result", "not computed yet!");
+                    return new ModelAndView(map, "compute.mustache");
+                },
+                new MustacheTemplateEngine());
     }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 
     public static String repeat(ArrayList<Integer> intList, ArrayList<Integer> intList2 ,ArrayList<String> strList, ArrayList<String> strList2){
         if(intList==null || intList2==null || strList==null || strList2==null){
